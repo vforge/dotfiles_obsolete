e01e7483238aa5aefacf5eb599a6438544bc2a0c
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2016-02-09 15:44:56 +0100

    add nvim init

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
 echo "copying git files..."
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
+
+echo "copying nvim config..."
+mkdir ~/.config/nvim
+cp nvim/init.vim ~/.config/nvim/init.vim
+
 echo "done."
+
